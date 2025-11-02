d550e7aa05a361e1504e264b3741ac243cde7185
devops2613 <msdevops2613@gmail.com>
2025-11-02 06:41:06 +0530

    Initial commit

---
+++ src/main/java/calciPkg/myCalci.java
@@ -0,0 +1,19 @@
+package calciPkg;
+
+public class myCalci {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int diff(int a, int b) {
+        return a - b;
+    }
+
+
+    public static void main(String[] args) {
+        myCalci calci = new myCalci();
+        System.out.println("sum is :"+ calci.sum(20, 10));
+        System.out.println("diff is :"+ calci.diff(20, 10));
+
+    }
+}
