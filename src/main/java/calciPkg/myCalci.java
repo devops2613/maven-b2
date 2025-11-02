ac6af9157d23cbef32058d51f468986bb25fc9fd
devops2613 <msdevops2613@gmail.com>
2025-11-02 07:20:28 +0530

    div added

--- src/main/java/calciPkg/myCalci.java
+++ src/main/java/calciPkg/myCalci.java
@@ -13,12 +13,19 @@ public class myCalci {
         return a*b;
     }
 
+    int div(int a, int b) {
+        return a-b;
+    }
+
+
+
 
     public static void main(String[] args) {
         myCalci calci = new myCalci();
         System.out.println("sum is :"+ calci.sum(20, 10));
         System.out.println("diff is :"+ calci.diff(20, 10));
         System.out.println("mul is :"+ calci.mul(20, 10));
+        System.out.println("div is :"+ calci.div(20, 10));
 
     }
 }
